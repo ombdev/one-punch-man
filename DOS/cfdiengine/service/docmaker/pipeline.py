ee6f830eb90173348b26f4e0098555ab843c2c44
Edwin Pĺauchu <pianodaemon@gmail.com>
2021-07-02 18:18:22 -0500

    pipeline

---
+++ DOS/cfdiengine/service/docmaker/pipeline.py
@@ -0,0 +1,16 @@
+from docmaker.gen import BuilderGen
+
+
+class DocPipeLine(object):
+    """
+    creator instance of documents.
+    """
+
+    def __init__(self, resdir = None, rdirs_conf = None):
+
+        if resdir == None:
+            raise DocBuilderError('resources directory not fed!!')
+        if rdirs_conf == None:
+            raise DocBuilderError("rdirs config info not fed!!")
+
+        self.resdir, self.rdirs_conf = resdir, rdirs_conf
