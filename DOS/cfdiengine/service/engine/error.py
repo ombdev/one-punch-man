1adff0590250d9bd50ab6494a78f1603e18af0da
Edwin Pĺauchu <pianodaemon@gmail.com>
2021-07-02 14:42:38 -0500

    API ERROR codes

---
+++ DOS/cfdiengine/service/engine/error.py
@@ -0,0 +1,21 @@
+import enum
+
+class ErrorCode(enum.Enum):
+    """
+    Contains error codes that shall be
+    communicated to consumer part of engine
+    """
+    SUCCESS = 0
+    # values from 1 up to 200 are reserved
+    # for answers of business handlers
+    RESOURCE_NOT_FOUND = 192  # An element searched into resources is not there
+    REQUEST_INVALID = 193     # It is not possible to consume request as it is comformed
+    ETL_ISSUES = 197          # ETL means extract, transform, and load
+    REQUEST_INCOMPLETE = 198  # Denotes a missing value in the request body expected
+    DOCMAKER_ERROR = 199      # Problems related to docmaker stuff
+    THIRD_PARTY_ISSUES = 200  # Lack interacting with third party entities
+
+    # values from 201 up to 255 are reserved
+    # for answers of engine mechanism
+    MOD_BUSINESS_NOT_LOADED = 201
+    MOD_BUSINESS_UNEXPECTED_FAIL = 202
