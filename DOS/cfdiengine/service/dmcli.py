1fbb865be210aba15ee289fe0754ff58080224d0
Edwin Pĺauchu <pianodaemon@gmail.com>
2021-07-03 14:18:12 -0500

    pretty

--- DOS/cfdiengine/service/dmcli.py
+++ DOS/cfdiengine/service/dmcli.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+#!/usr/bin/python3
+
 import argparse
 import logging
 import os
@@ -13,6 +15,7 @@ _logger = logging.getLogger(__name__)
 
 
 def _dmcli(args):
+    """docmaker cli implementation"""
 
     def env_property(prop):
         """Read env variables for microservice's sake"""
@@ -22,28 +25,29 @@ def _dmcli(args):
         raise FatalError("Enviroment variable {} has not been set !!".format(prop))
 
     def read_settings(s_file):
-        _logger.debug("looking for config profile file in:\n{0}".format(
-            os.path.abspath(s_file)))
+        _logger.debug(
+            "looking for config profile file in:\n{0}".format(os.path.abspath(s_file))
+        )
         if os.path.isfile(s_file):
             return profile_read(s_file)
         raise FatalError("unable to locate the config profile file")
 
-    RESOURCES_DIR = os.path.join(env_property('BASE_DIR'), 'resources')
+    RESOURCES_DIR = os.path.join(env_property("BASE_DIR"), "resources")
 
     if not os.path.isdir(RESOURCES_DIR):
-        msg = 'We can not go ahead without a resource directory'
+        msg = "We can not go ahead without a resource directory"
         sys.exit(msg)
 
-    PROFILES_DIR = os.path.join(RESOURCES_DIR, 'profiles')
+    PROFILES_DIR = os.path.join(RESOURCES_DIR, "profiles")
 
     if not os.path.isdir(PROFILES_DIR):
-        msg = 'We can not go ahead without a profile directory'
+        msg = "We can not go ahead without a profile directory"
         sys.exit(msg)
 
-    PROFILE_PATH = '{}.yaml'.format(os.path.join(PROFILES_DIR, args.config))
+    PROFILE_PATH = "{}.yaml".format(os.path.join(PROFILES_DIR, args.config))
 
     if not os.path.exists(PROFILE_PATH):
-        msg = 'We can not go ahead without a profile'
+        msg = "We can not go ahead without a profile"
         sys.exit(msg)
 
     pt = read_settings(PROFILE_PATH)
@@ -57,20 +61,25 @@ def _dmcli(args):
 
         kwargs = dict([])
         try:
-            if args.dm_input.find(';') == -1:
+            if args.dm_input.find(";") == -1:
                 raise Exception("input variables bad conformed")
             else:
-                for opt in args.dm_input.split(';'):
-                    if opt.find('=') == -1:
+                for opt in args.dm_input.split(";"):
+                    if opt.find("=") == -1:
                         continue
-                    (k , v) = opt.split('=')
+                    (k, v) = opt.split("=")
                     kwargs[k] = v
         except ValueError:
             raise FatalError("input variables bad conformed")
 
         try:
-            builder(RESOURCES_DIR, pt['res']['dirs'],
-                    args.dm_builder, args.dm_output, **kwargs)
+            builder(
+                RESOURCES_DIR,
+                pt["res"]["dirs"],
+                args.dm_builder,
+                args.dm_output,
+                **kwargs,
+            )
         except:
             raise FatalError("problems in document builder")
     else:
@@ -80,20 +89,36 @@ def _dmcli(args):
 def _set_cmdargs_up():
     """parses the cmd line arguments at the call"""
 
-    psr_desc='Document maker command line control interface'
-    psr_epi='The config profile is used to specify defaults'
+    psr_desc = "Document maker command line control interface"
+    psr_epi = "The config profile is used to specify defaults"
 
     psr = argparse.ArgumentParser(description=psr_desc, epilog=psr_epi)
 
-    psr.add_argument('-d', '--debug', action='store_true',
-                     dest='dm_debug', help='print debug information')
-    psr.add_argument('-c', '--config', action='store', default='basic',
-                     dest='config', help='load an specific config profile')
-    psr.add_argument('-b', '--builder',
-                     dest='dm_builder', help='specify the builder to use')
-    psr.add_argument('-i', '--input', dest='dm_input',
-                     help='specify the input variables with \'var=val;var2=val2;var2=valN\'..')
-    psr.add_argument('-o', '--output', dest='dm_output', help='specify the output file')
+    psr.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",
+        dest="dm_debug",
+        help="print debug information",
+    )
+    psr.add_argument(
+        "-c",
+        "--config",
+        action="store",
+        default="basic",
+        dest="config",
+        help="load an specific config profile",
+    )
+    psr.add_argument(
+        "-b", "--builder", dest="dm_builder", help="specify the builder to use"
+    )
+    psr.add_argument(
+        "-i",
+        "--input",
+        dest="dm_input",
+        help="specify the input variables with 'var=val;var2=val2;var2=valN'..",
+    )
+    psr.add_argument("-o", "--output", dest="dm_output", help="specify the output file")
 
     return psr.parse_args()
 
@@ -106,7 +131,7 @@ if __name__ == "__main__":
         logging.basicConfig(level=logging.DEBUG if args.dm_debug else logging.INFO)
         _logger.debug(args)
         _dmcli(args)
-        _logger.info('successful builder execution')
+        _logger.info("successful builder execution")
     except:
         if args.dm_debug:
             traceback.print_exc()
