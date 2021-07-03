aad565377c895431d573fa6025bdfadc153a7672
Bob Ross <pianodaemon@gmail.com>
2021-07-03 11:52:21 -0500

    Update profconf.py

--- DOS/cfdiengine/service/custom/profconf.py
+++ DOS/cfdiengine/service/custom/profconf.py
@@ -1,28 +1,10 @@
 import yaml
-import os
-from engine.error import FatalError
-
-
-def env_property(prop, caster=None):
-    '''Read env variables for microservice's sake'''
-
-    val = os.environ.get(prop)
-    if val is None:
-        raise FatalError("Enviroment variable {} has not been set !!".format(prop))
-
-    if caster is None:
-       return val
-
-    try:
-        return caster(val)
-    except:
-        raise FatalError("Enviroment variable {} could not be casted !!".format(prop))
 
 
 def profile_read(p_file_path):
     """
-    create a profile tree as per
-    a determined config profile
+    create a tree of dictioanaries as per
+    a profile determined
     """
 
     def parse_profile():
