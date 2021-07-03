fc55f2c783f391b676fabb07914c4096243158ad
Edwin Pĺauchu <pianodaemon@gmail.com>
2021-07-03 16:51:33 -0500

    fix typo

--- DOS/cfdiengine/service/docmaker/builder.py
+++ DOS/cfdiengine/service/docmaker/builder.py
@@ -2,7 +2,7 @@ from docmaker.gen import BuilderGen
 from docmaker.error import DocBuilderImptError, DocBuilderStepError, DocBuilderError
 
 
-def builder(self, resdir, rdirs_conf, bid, f_outdoc **kwargs):
+def builder(self, resdir, rdirs_conf, bid, f_outdoc, **kwargs):
 
     if resdir == None:
         raise DocBuilderError('resources directory not fed!!')
